f41296193bc9549b63b9214cd7928fb024630bf3
Harry <Harry_emmett@outlook.com>
2022-01-14 15:18:34 +0000

    added file

--- src/component/Bar.jsx
+++ src/component/Bar.jsx
@@ -9,7 +9,7 @@ const Bar = () => {
     const [loaded, setLoaded] = useState(false);
     let tempId;
     useEffect(() => {
-        axios.get(`mongodb://localhost:27017/beerCollection`)
+        axios.get(`http://localhost:5015/beer/readAll`)
             .then((response) => {
                 setBeerData(response.data);
                 console.log("===========================");
@@ -25,16 +25,43 @@ const Bar = () => {
                 console.log(beerData);
             });
     }, []);
-    // const getBeerId = (id) => {
-    //     axios.get(`mongodb://localhost:27017/beerCollection/${id}`)
-    //         .then((response) => {
-    //             setBeerData(response.data);
-    //             setLoaded(true);
-    //         }).catch((error) => {
-    //             setLoaded(true);
-    //             setError(error);
-    //         });
-    // }
+
+    const getBeerId = (id) => {
+        axios.get(`http://localhost:5015/beer/read/${id}`)
+            .then((response) => {
+                console.log("read by id yes connor");
+                console.log(response);
+                console.log(response.data);
+                console.log(response.data.beerName)
+                setBeerData([response.data]); //need to turn into an array otherwise you cant .map in the return 
+                setLoaded(true);
+            }).catch((error) => {
+                setLoaded(true);
+                setError(error);
+            }).then(() => {
+                console.log(beerData);
+            });
+    }
+
+    const deleteBeerId = (id) => {
+        axios.delete(`http://localhost:5015/beer/delete/${id}`)
+        .then((response) => {
+            console.log(response);
+            console.log(`item deleted`);
+        })
+        .catch((error) => {
+            setLoaded(true);
+            setError(error);
+        })
+
+    }
+
+    const postBeer = () => {
+        axios.post(`http://localhost:5015/beer/create`)
+
+
+
+    }
 
     if (error === true) {
         return <h2>Oops,theres been an error please refresh the page</h2>
@@ -44,17 +71,52 @@ const Bar = () => {
         return (
             <div>
                 <h2>Loads if the data is fine</h2>
-                {/* <input type="number" name="beerId" onChange={(event) => tempId = (event.target.value)} /> */}
-                {/* <button onClick={() => { getBeerId(tempId) }}>
-                    Get Beer by Id
-                </button> */}
+                <input type="text" name="beerName" onChange={(event) => tempId = (event.target.value)} />
+                <button type="button" onClick={() => { getBeerId(tempId) }}>
+                    BeerName
+                </button>
+                <input type="text" name="deleteId" onChange={(event) => tempId = (event.target.value)} />
+                <button type="button" onClick={() => { deleteBeerId(tempId) }}>
+                    Delete beer by Id
+                </button>
+
+                <form>
+                <label for="beerName">beer name</label>
+                <input type="text" name="beerName" onChange={(event) => tempId = (event.target.value)} />
+                <br></br>
+                <label for="description">description</label>
+                <input type="text" name="description" onChange={(event) => tempId = (event.target.value)} />
+                <br></br>
+                <label for="image">image</label>
+                <input type="text" name="image" onChange={(event) => tempId = (event.target.value)} />
+                <br></br>
+                <label for="abv">abv</label>
+                <input type="text" name="abv" onChange={(event) => tempId = (event.target.value)} />
+                <br></br>
+                <label for="alcoholFree">alcohol free</label>
+                <input type="text" name="alcoholFree" onChange={(event) => tempId = (event.target.value)} />
+                <br></br>
+                <label for="beerType">beerType</label>
+                <input type="text" name="beerType" onChange={(event) => tempId = (event.target.value)} />
+                <br></br>
+                
+                <button type="submit" onClick={() => { createBeer(tempId) }}>
+                    Create beer
+                </button>
+                <button type="reset">
+                    Reset
+                </button>
+
+                </form>
+
                 {beerData.map((beer) => {
-                    return <Beer beerName={beer.beerName}/>
-                })};
+                    console.log(beer);
+                    console.log(beer.beerName);
+                    return <Beer beerName = {beer.beerName} description= {beer.description} image={beer.imageUrl} abv={beer.abv} alcoholFree={beer.alcoholFree.toString()} beerType={beer.beerType} />
+                })}
             </div>
         )
     }
 }
 export default Bar;
 
-// description= {beer.description} image={beer.imageUrl} abv={beer.abv} alcoholFree={beer.alcoholFree} beerType={beer.beerType} 
--- src/component/Beer.jsx
+++ src/component/Beer.jsx
@@ -1,7 +1,12 @@
-const MyBeer = ({name}) => {
+const MyBeer = ({beerName, description, image, abv, alcoholFree, beerType}) => {
     return ( 
         <div>
-        <h2>name of beer: {name}</h2>
+        <h2>name of beer: {beerName}</h2>
+        <h2>name of description: {description}</h2>
+        <h2>name of image: {image}</h2>
+        <h2>name of abv: {abv}</h2>
+        <h2>name of alcoholFree: {alcoholFree}</h2>
+        <h2>name of beerType: {beerType}</h2>
         </div>
      );
 }
